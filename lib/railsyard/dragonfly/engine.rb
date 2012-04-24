920d787602a56d6b327b1d883e299b9fabc65f11
Stefano Verna <stefano.verna@welaika.com>
2012-04-24 15:59:50 +0200

    Dependent on railsyard-backend

--- lib/railsyard/dragonfly/engine.rb
+++ lib/railsyard/dragonfly/engine.rb
@@ -2,7 +2,7 @@ module Railsyard
   module Dragonfly
     class Engine < ::Rails::Engine
       config.to_prepare do
-        Railsyard.plugin_manager.add_plugin(:dragonfly) do
+        Railsyard::Backend.plugin_manager.add_plugin(:dragonfly) do
           name "Dragonfly"
           backend_javascript_dependency "railsyard/admin/railsyard_dragonfly"
           backend_stylesheet_dependency "railsyard/admin/railsyard_dragonfly"
