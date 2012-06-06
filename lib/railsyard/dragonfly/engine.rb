e27597514098e49bf965d92398e541ee4469eb6c
Stefano Verna <stefano.verna@welaika.com>
2012-06-06 17:49:39 +0200

    Added precompilation asset

--- lib/railsyard/dragonfly/engine.rb
+++ lib/railsyard/dragonfly/engine.rb
@@ -1,6 +1,14 @@
 module Railsyard
   module Dragonfly
     class Engine < ::Rails::Engine
+
+      initializer "Railsyard precompile hook", :group => :assets do |app|
+        app.config.assets.precompile += [
+          "railsyard/admin/railsyard_dragonfly.css",
+          "railsyard/admin/railsyard_dragonfly.js"
+        ]
+      end
+
       config.to_prepare do
         Railsyard::Backend.plugin_manager.add_plugin(:dragonfly) do
           name "Dragonfly"
