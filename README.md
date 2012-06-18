011cadf54123f19701536ac581ce3d07c3a33b63
nebirhos <nebirhos@aol.com>
2012-06-18 18:22:26 +0200

    Support for custom generator

--- README.md
+++ README.md
@@ -9,10 +9,16 @@ Add a Dragonfly attachment to your backend resource.
 
         gem "railsyard-dragonfly"
 
-2. Configure Dragonfly initializer and migration (see https://github.com/markevans/dragonfly)
+## Usage
 
-3. Backend configuration (e.g. app/backend/editor_for_books.rb):
+1. Configure Dragonfly initializer and migration (see https://github.com/markevans/dragonfly)
+
+2. Backend configuration (e.g. app/backend/editor_for_books.rb):
 
         field :image, as: :dragonfly do
           input_options components: [ :preview, :upload, :url, :remove ] # use any of these
         end
+
+... or just use the generator:
+
+    rails g railsyard:editor Book title:string image:dragonfly
--- lib/railsyard/dragonfly/engine.rb
+++ lib/railsyard/dragonfly/engine.rb
@@ -10,10 +10,22 @@ module Railsyard
       end
 
       config.to_prepare do
+        editor_config = <<DSL
+field :%{name}, as: :dragonfly do
+      input_options components: [ :preview, :upload, :url, :remove ] # any of these
+    end
+DSL
+        model_config = <<MODEL
+  image_accessor :%{name}
+MODEL
         Railsyard::Backend.plugin_manager.add_plugin(:dragonfly) do
           name "Dragonfly"
           backend_javascript_dependency "railsyard/admin/railsyard_dragonfly"
           backend_stylesheet_dependency "railsyard/admin/railsyard_dragonfly"
+          generator_editor_type(:dragonfly,
+                                {"%{name}_uid" => :string, "%{name}_name" => :string},
+                                editor_config,
+                                model_config)
         end
       end
     end
